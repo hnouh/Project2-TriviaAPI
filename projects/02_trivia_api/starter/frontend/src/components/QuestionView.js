c5b75fa6d588b29143b740bb11e022c2b1094c1e
hanin <haninnouh@gmail.com>
2021-01-14 23:33:55 +0300

    quiz+readme+test

--- projects/02_trivia_api/starter/frontend/src/components/QuestionView.js
+++ projects/02_trivia_api/starter/frontend/src/components/QuestionView.js
@@ -78,7 +78,7 @@ class QuestionView extends Component {
 
   submitSearch = (searchTerm) => {
     $.ajax({
-      url: `/questions`, //TODO: update request URL
+      url: `/searchQuestions`, //TODO: update request URL
       type: "POST",
       dataType: 'json',
       contentType: 'application/json',
